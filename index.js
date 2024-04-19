7e6af324993d7e1a02c42a34fab33dbbdb55d86c
rjsanghamitra <rjsanghamitra@gmail.com>
2024-04-20 03:44:00 +0530

    added cors and vercel.json

--- index.js
+++ index.js
@@ -3,9 +3,16 @@ const app = express();
 const dotenv = require("dotenv").config();
 const mongoose = require("mongoose");
 const authRoutes = require("./routes/auth.js");
+const cors = require("cors");
 const userRoutes = require("./routes/display.js");
 
 app.use(express.json());
+
+app.use(cors({
+    origin: "https://user-registration-five.vercel.app/",
+    methods: ["GET", "POST"],
+    credentials: true,
+}));
 
 app.use("/auth", authRoutes);
 app.use("/", userRoutes);
